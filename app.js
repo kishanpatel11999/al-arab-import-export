f5e9c102ec2a9489cce27da23ed0a6c29e2726d6
kishanlakhani <kishan.lakhani@bacancytechnology.com>
2020-01-01 22:44:28 +0530

    css commit

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ app.post('/form',(req,res)=>{
     res.redirect('/contact');
 })
 
-const PORT = process.env.PORT || 3005;
+const PORT = process.env.PORT || 3010;
 app.listen(PORT,(req,res)=>{
     console.log(`server started in port on ${PORT}...`);
 })
